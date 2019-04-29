38a932a1a1f82c37f0c43ba280fd33464fce26e0
Yormax <yormax@Yormaxs-MacBook-Pro.local>
2019-04-28 23:08:40 -0400

    added a couple things to the event component

--- src/components/Event.js
+++ src/components/Event.js
@@ -7,7 +7,7 @@ import "./event.css";
 
 class Event extends Component {
 
- constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       startDate: new Date(),
@@ -31,65 +31,79 @@ class Event extends Component {
   }
 
 
-    render() {
-        return (
-            <div>
-                <form>
-                    <ul class="horizontalRow">
-                        <li>
-                            <input type="text" placeholder="Event Name"></input>
-                        </li>
-                        <li>
-                            <input type="submit" value="Save"></input>
-                        </li>
+  render() {
+    return (
+      <div>
+        <form>
+          <ul class="horizontalRow">
+            <li>
+              <h1>Event Details</h1>
+            </li>
+            <li>
+              <input type="text" placeholder="Event Name"></input>
+            </li>
+            <li>
+              <input type="submit" value="Save"></input>
+            </li>
 
-                        <br></br>
-                        <br></br>
-                        <li> 
-                            <DatePicker
-                            selected={this.state.startDate}
-                            onChange={this.handleStartChange}
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                            timeIntervals={15}
-                            dateFormat="MMMM d, yyyy h:mm aa"
-                            timeCaption="time"
-                            /> 
-                        </li>
-                        <li> to </li>
-                        <li>                     
-                            <DatePicker
-                            selected={this.state.endDate}
-                            onChange={this.handleEndChange}
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                            timeIntervals={15}
-                            dateFormat="MMMM d, yyyy h:mm aa"
-                            timeCaption="time"
-                            />
-                        </li>
-                        <li>
-                            <h1>Event Details</h1>
-                        </li>
+            <br></br>
+            <br></br>
+            <li>
+              <label for="dateRange">Date :</label>
+              <DatePicker
+                name="dateRange"
+                selected={this.state.startDate}
+                onChange={this.handleStartChange}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                timeCaption="time"
+              />
+            </li>
+            <li> to </li>
+            <li>
+              <DatePicker
+                selected={this.state.endDate}
+                onChange={this.handleEndChange}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                timeCaption="time"
+              />
+            </li>
+            <br></br>
+            <br></br>
+            <li>
+              <label for="location"> Location: </label>
+              <input type="text" name="location" placeholder="Ex. Menlo Park" />
+            </li>
+            <br></br>
+            <br></br>
+            <li>
+              <label for="description">Description:</label>
+              <br></br>
+              <br></br>
+              <textarea name="description"></textarea>
+            </li>
 
-                        <li>
-                                <label for="location"> </label>
-                                <input type="text" name="location" placeholder="Ex. Menlo Park"/> 
-                            
-                        </li>
-                    </ul>
+            <li> <p>Guest List</p> </li>
 
 
-                    <p> Day to Day</p>
-                    <p>Location</p>
-                    <p>Description</p>
-                    <p>guest</p>
+          </ul>
 
-                </form>
-                
-            </div>
-        );
-    }
+
+          <p> Day to Day</p>
+          <p>Location</p>
+          <p>Description</p>
+          <p>guest</p>
+
+        </form>
+
+      </div>
+    );
+  }
 }
 
 export default Event;
