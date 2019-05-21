e7c3daed6db66b715873bce48e8dd36d2252f3cd
Yormax <yormax.castro@accenturefederal.com>
2019-05-21 16:14:21 -0400

    restructing calendar prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Event from './components/Event';
+import events from 'events';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 import Calendar from './components/Calendar';
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import './App.css';
 
+
+const localizer = BigCalendar.momentLocalizer(moment);
+const MyCalendar = props => ( <div>
+  < BigCalendar localizer = {
+    localizer
+  }
+  events = { events }
+  startAccessor = "01/01/2000"
+  endAccessor = "01/30/2001" / >
+</div>
+);
+
 function App() {
+  ReactDOM.render(
+    MyCalendar,
+    document.getElementById('root')
+  );
+  console.log(localizer);
   return (
+    
     <div className="App">
-      {/* <Event/> */}
-      <Calendar/>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,41 +1,26 @@
 import React, { Component } from 'react';
-import BigCalendar from 'react-big-calendar';
-import moment from 'moment';
+import PropTypes from 'prop-types'
 import 'moment-timezone';
 import "./calendar.css";
 
 
 class Calendar  extends Component {
     
-
-    state={  
-        localizer: BigCalendar.momentLocalizer(moment) //  or globalizeLocalizer
-    }
-
     
-
-
-
-
     render() {
         return (
             <div>
-                    < BigCalendar
-                    localizer = {
-                        localizer
-                    }
-                    events = {
-                        myEventsList
-                    }
-                    startAccessor = "1/1/2001"
-                    endAccessor = "1/30/2001" /
-                        >
 
+             <p>this is { this.props.name }</p>
+             <p> this is { this.props.age } </p>
 
             </div>
-
+ 
         );
     }
 }
-
+Calendar.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number
+}
 export default Calendar;
