fe2e017b548de1b684e9c546293e514528c3e66a
Yormax <yormax.castro@accenturefederal.com>
2019-05-21 13:54:36 -0400

    making calendar a prop

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import 'moment-timezone';
 import "./calendar.css";
 
 
 class Calendar  extends Component {
+    
+
+    state={  
+        localizer: BigCalendar.momentLocalizer(moment) //  or globalizeLocalizer
+    }
+
+    
+
+
+
+
     render() {
         return (
-            <div id="container">
+            <div>
+                    < BigCalendar
+                    localizer = {
+                        localizer
+                    }
+                    events = {
+                        myEventsList
+                    }
+                    startAccessor = "1/1/2001"
+                    endAccessor = "1/30/2001" /
+                        >
+
 
             </div>
+
         );
     }
 }
