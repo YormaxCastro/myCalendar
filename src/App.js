a88b684d2fe49c3493ff85b218f3e14d4a3d8a57
Yormax <yormax.castro@accenturefederal.com>
2019-05-22 10:12:39 -0400

    calendar appears

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Event from './components/Event';
-import events from 'events';
+import events from './events';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import Calendar from './components/Calendar';
@@ -10,25 +10,18 @@ import './App.css';
 
 
 const localizer = BigCalendar.momentLocalizer(moment);
-const MyCalendar = props => ( <div>
-  < BigCalendar localizer = {
-    localizer
-  }
-  events = { events }
-  startAccessor = "01/01/2000"
-  endAccessor = "01/30/2001" / >
-</div>
-);
 
 function App() {
-  ReactDOM.render(
-    MyCalendar,
-    document.getElementById('root')
-  );
-  console.log(localizer);
+  console.log(events);
+
+
   return (
     
     <div className="App">
+      <BigCalendar 
+      events= { events }
+      localizer= { localizer }
+      />
     </div>
   );
 }
