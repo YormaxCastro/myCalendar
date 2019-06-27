e54499ea383911bc7052aa73f770e66cf5e6c61f
Yormax <yormax@Yormaxs-MacBook-Pro.local>
2019-06-26 21:36:44 -0400

    made a task container

--- src/components/template.js
+++ src/components/template.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import EventsList from '../containers/events-list';
 const View = () => (
   <div>
     <h2>UserName:</h2>
+    <EventsList></EventsList>
     <hr></hr>
     <h2>Task:</h2>
   </div>
---
+++ src/containers/events-list.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+class EventsList extends Component {
+
+  render() {
+    return (
+      <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    events: state.events
+  }
+
+}
+
+export default connect(mapStateToProps)(EventsList);
