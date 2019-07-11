413edfa6476680045d091afc97413952490d006c
YormaxCastro <yormax_castro@hotmail.com>
2019-07-10 23:29:27 -0400

    attempting to add redux to active event

--- src/components/eventComponent.jsx
+++ src/components/eventComponent.jsx
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import { tsThisType } from '@babel/types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { selectEvent } from '../actions/index';
 
- class eventComponent  extends Component {
-     constructor(props){
-         super(props);
-         this.state={
-             title: props.title
-         }
-         this.onClick = this.onClick.bind(this);
-        
-     }
+class eventComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: props.title
+        }
+        this.onClick = this.onClick.bind(this);
 
-     onClick= ()=> {
-         console.log("onCLick fired for "+ this.props.title);
-     }
+    }
+
+    onClick = () => {
+        console.log("onCLick fired for " + this.props.title);
+        console.log(this.state);
+    }
 
     render() {
         return (
             <div class="eventRibbon" >
-                <p onClick={ ()=> { this.onClick() } }> { this.state.title } </p>
+                <p onClick={() => { this.onClick() }}> {this.state.title} </p>
             </div>
-                
+
         );
     }
 }
+function mapStateToProps(state) {
+    console.log(state);
+    return { activeEvent: state.activeEvent };
+}
+function matchDispatchToProps(dispatch) {
+    return bindActionCreators({ selectEvent: selectEvent }, dispatch)
+}
 
-export default eventComponent;
+export default connect(mapStateToProps, matchDispatchToProps)(eventComponent);
+
