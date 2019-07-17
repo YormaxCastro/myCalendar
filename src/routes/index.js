0e4dd4c86441f10f532a7b098dccc180f7ab262c
YormaxCastro <yormax_castro@hotmail.com>
2019-07-16 22:12:05 -0400

    made routes folder

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Hoome from './Home'; // Make this commponent around the 3:31 mark 
+
+export default () => (
+  <BrowserRouter>
+    <Route path="/" component={Home} />
+  </BrowserRouter>
+);
+
