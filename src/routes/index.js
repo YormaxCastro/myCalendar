59385114384355e19003cf1da404adda588a4f6f
YormaxCastro <yormax_castro@hotmail.com>
2019-07-17 23:07:17 -0400

    added comments to add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import Hoome from './Home'; // Make this commponent around the 3:31 mark
 export default () => (
   <BrowserRouter>
     <Route path="/" component={Home} />
+    {/* add all other routes */}
   </BrowserRouter>
 );
 
